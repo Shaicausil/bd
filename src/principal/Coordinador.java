435b99b69e2f54b195196e65f545e58eda6653ce
Shaira Causil <causil845@gmail.com>
2025-06-04 17:53:54 -0500

    Update Coordinador.java

--- src/principal/Coordinador.java
+++ src/principal/Coordinador.java
@@ -1,4 +1,23 @@
 package principal;
 
+import dao.PersonaDao;
+import vo.PersonaVo;
+
+import java.util.ArrayList;
+
 public class Coordinador {
+
+    private PersonaDao personaDao;
+
+    public Coordinador() {
+        personaDao = new PersonaDao();
+    }
+
+    public void registrarPersona(PersonaVo persona) {
+        personaDao.registrarPersona(persona);
+    }
+
+    public ArrayList<PersonaVo> consultarListaPersonas() {
+        return personaDao.obtenerListaPersonas();
+    }
 }
