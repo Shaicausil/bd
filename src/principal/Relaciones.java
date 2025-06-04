b0093e056cd3971991fba6f57fa07b81a819b935
Shaira Causil <causil845@gmail.com>
2025-06-04 17:54:28 -0500

    Update Relaciones.java

--- src/principal/Relaciones.java
+++ src/principal/Relaciones.java
@@ -5,14 +5,12 @@ import ventanas.VentanaCombo;
 public class Relaciones {
 
     public void iniciar() {
+        VentanaCombo miVentanaCombo = new VentanaCombo();
+        Coordinador miCoordinador = new Coordinador();
 
-        VentanaCombo miVentanaCombo=new VentanaCombo();
-        Coordinador miCoordinador=new Coordinador();
+        miVentanaCombo.setCoordinador(miCoordinador);
+        miVentanaCombo.cargarRegistros();
 
-        //miVentanaCombo.setCoordinador(miCoordinador);
-        //miCoordinador.setVentanaCombo(miVentanaCombo);
-
-       // miCoordinador.cargarRegistro();
-        miVentanaCombo.setVisible((true));
+        miVentanaCombo.setVisible(true);
     }
 }
